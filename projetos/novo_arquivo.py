07aa84c965a61350f1cd5a7deddb18144d879154
Maicon Aparecido Micalowski <micalowski@gmail.com>
2020-05-29 22:17:35 -0300

    update novo_arquivo.py

--- projetos/novo_arquivo.py
+++ projetos/novo_arquivo.py
@@ -1,3 +1,7 @@
-nome_pc = str(input("Digite seu nome:")).upper()
+data = str(input("Informe a data: "))
+hora = str(input("Informe a hora: "))
+nome_pc = str(input("Digite o nome do paciente: ")).upper()
 menor = 'S'
 print(f'Olá {nome_pc} tudo bem com você?')
+nome_ac = str(input("Digite o nome do acopanhante")).upper()
+
