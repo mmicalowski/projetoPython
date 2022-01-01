efb812208e985a4489c0e0bdb5ea4c3af3f66dc0
Maicon Aparecido Micalowski <micalowski@gmail.com>
2022-01-01 08:04:45 -0300

    Update novo_arquivo.py

--- projetos/novo_arquivo.py
+++ projetos/novo_arquivo.py
@@ -1,7 +1,14 @@
+nome = input ("Digite seu nome: ").title()
+idade = int(input("Digite sua idade: "))
+print (nome)
+if idade == 41:
+  print ("Que nome lindo")
+  print ("Tem certeza que sua idade é essa?")
+  if nome == 'Maicon':
+    print ('Oi gatão')
 data = str(input("Informe a data: "))
 hora = str(input("Informe a hora: "))
 nome_pc = str(input("Digite o nome do paciente: ")).upper()
 menor = 'S'
-print(f'Olá {nome_pc} tudo bem com você?')
-nome_ac = str(input("Digite o nome do acopanhante")).upper()
-
+print(f'Paciente {nome_pc} é Menor?')
+nome_ac = str(input("Digite o nome do acopanhante: ")).upper()
